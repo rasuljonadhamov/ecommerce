c80e49e5f698762954092551c20bf801633b9333
rasuljonadhamov <rasuljonadhamov@gmail.com>
2024-02-01 02:06:14 +0500

    fixed produc listing

--- src/components/ProductListing/ProductListing.jsx
+++ src/components/ProductListing/ProductListing.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
-import SearchBar from "../SearchBar/SearchBar";
 import PriceFilter from "../PriceFilter/PriceFilter";
 import ProductList from "../ProductList/ProductList";
 import "./ProductListing.scss";
+import { Input, Select } from "antd";
 
 const ProductListing = ({ json, products, isDarkMode }) => {
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [priceQuery, setPriceQuery] = useState(1000);
+  const [selectedCategory, setSelectedCategory] = useState("all");
+
+  const categories = ["all", "tables", "chairs", "kids", "sofas", "beds"];
 
   function handleSearch(e) {
     console.log(products);
@@ -28,18 +31,36 @@ const ProductListing = ({ json, products, isDarkMode }) => {
     console.log(39, filteredProducts);
   };
 
+  const handleCategoryChange = (value) => {
+    setSelectedCategory(value);
+  };
+
   return (
     <div className={`product-listing ${isDarkMode ? "dark-mode" : ""}`}>
-      <SearchBar
-        searchQuery={searchQuery}
-        setSearchQuery={setSearchQuery}
-        handleSearch={handleSearch}
+      <Input.Search
+        placeholder="Search"
+        style={{ width: 250, marginRight: 10, marginBottom: 20 }}
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        onSearch={handleSearch}
       />
+      <Select
+        defaultValue="all"
+        style={{ width: 120 }}
+        onChange={handleCategoryChange}
+      >
+        {categories.map((category) => (
+          <Select.Option key={category} value={category}>
+            {category}
+          </Select.Option>
+        ))}
+      </Select>
       <PriceFilter
         priceQuery={priceQuery}
         setPriceQuery={setPriceQuery}
         handlePriceFilter={handlePriceFilter}
       />
+
       <ProductList filteredProducts={filteredProducts} products={products} />
     </div>
   );
